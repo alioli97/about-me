b2b9e435fc3c9635410d41ede9e1a5eef4c90a61
Arnau Alio <arnau.alio.fcb@gmail.com>
2026-04-03 12:21:14 +0200

    Update base URL to about-me

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: '/my-cv-interactive/'
+  base: '/about-me/' // 🟢 AHORA SE LLAMA ASÍ
 })
